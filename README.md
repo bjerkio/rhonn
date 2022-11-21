ae0fefa81b00f48cc365fc2bb74df6f2b86f1b18
Brage Sekse Aarset <brage@bjerk.io>
2022-11-21 20:34:17 +0200

    feat: implement gameplay (#2)
    * feat: setup cargo package and hello world
    
    * feat: implement basic sandbox gameplay loop with actions and saving
    
    * docs: add some gameplay concepts to readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rhonn
+
 Rhonn: It's where the broccolis gather
+
+# Concepts
+
+navigation in space and narrative is represented by graphs
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use text_io::read;
+use yansi::Paint;
+
 fn main() {
     println!("Hello, world!");
-}
+    let mut playing = true;
+    let mut save: String = String::new();
+
+    while playing {
+
+        let action: String = read!();
+
+        if action == "exit" {
+            println!("{}", Paint::blue("Thank you for playing Rhonn"));
+            println!("Here is your save:{}", Paint::green(&save));
+            playing = false;
+
+        } else {
+            save.push_str(" ");
+            save.push_str(&action);
+            println!("Action performed: {}", Paint::green(action));
+        }
+
+    }
